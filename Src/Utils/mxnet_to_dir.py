900367549af8e83164f039dffe451a6af2cf5365
Ali Hassani <47923056+AHassani92@users.noreply.github.com>
2023-02-05 15:59:19 -0500

    Initial commit

---
+++ Src/Utils/mxnet_to_dir.py
@@ -0,0 +1,91 @@
+import numbers
+import os
+
+from PIL import Image
+import multiprocessing as mp
+import cv2 as cv
+import mxnet as mx
+import numpy as np
+
+def data_write_mp(write_root, label, index, image):
+    
+    # determine the proper directory
+    directory = 'Participant_'+str(int(label)).zfill(5)
+    
+    # generate a specific experiment repo if it doesn't exist:
+    directory = os.path.join(write_root, directory)
+    if not os.path.exists(directory):
+        os.makedirs(directory)   
+    
+    im_name = 'im_' + str(index).zfill(3) + '.png'
+    im_path = os.path.join(directory, im_name)
+    
+    image.save(im_path)
+
+# function to parse mxnet records into a  traditional data repo
+def mxnext_to_dir(data_root, write_root = None):
+    
+    if write_root == None:
+        write_root = data_root
+        
+    # generate the write directory to be safe
+    if not os.path.exists(write_root):
+        os.makedirs(write_root)
+        
+    # Mxnet RecordIO
+    path_imgrec = os.path.join(data_root, 'train.rec')
+    path_imgidx = os.path.join(data_root, 'train.idx')
+    imgrec = mx.recordio.MXIndexedRecordIO(path_imgidx, path_imgrec, 'r')
+    s = imgrec.read_idx(0)
+
+    header, _ = mx.recordio.unpack(s)
+    if header.flag > 0:
+        header0 = (int(header.label[0]), int(header.label[1]))
+        imgidx = np.array(range(1, int(header.label[0])))
+    else:
+        imgidx = np.array(list(self.imgrec.keys))
+
+    prev_label = 0
+    idx_offset = 0
+    
+    pool = mp.Pool(mp.cpu_count())
+    
+    #print('Processing MX Net Ledger with {} CPUS'.format(mp.cpu_count()))
+    print('Processing MX Net Ledger')
+    #for index in range(len(imgidx)):
+    for index in range(10):
+        idx = imgidx[index]
+        s = imgrec.read_idx(idx)
+        header, mx_img = mx.recordio.unpack(s)
+        label = header.label
+        if not isinstance(label, numbers.Number):
+            label = label[0]
+        
+        # smarter label indexing per person
+        if prev_label != label:
+            prev_label = label
+            idx_offset = idx
+        
+        image = mx.image.imdecode(mx_img).asnumpy()
+        image = Image.fromarray(image) 
+    
+        # call the multiprocessing write function
+        #data_write_mp(write_root, label, idx - idx_offset, mx_img)
+        #pool.apply_async(data_write_mp,  args=(write_root, label, idx - idx_offset, mx_img))
+        pool.apply_async(data_write_mp,  args=(write_root, label, idx - idx_offset, image))
+        
+        if index == 0 or index % 10000 == 0:
+            print('Adding index {} with label {}'.format(index, label))
+            #print('Adding index {} to MP pool with label {}'.format(index, label))
+                
+    print('Executing pool')
+    pool.close()
+    pool.join()
+
+
+
+data_root = 'F:\\Data\\faces_emore\\faces_emore'
+write_root = 'F:\\Data\\MS1M'
+
+
+mxnext_to_dir(data_root, write_root)
